5f6feb10cae963e439ea58cf19cc581801bd46ce
Mateus <teuspersi@gmail.com>
2021-07-03 14:43:12 -0300

    Altera CRUD para token no banco de dados

--- app/controllers/api/heroes_controller.rb
+++ app/controllers/api/heroes_controller.rb
@@ -1,24 +1,28 @@
 class Api::HeroesController < ApplicationController
   include Authenticable
 
-  before_action :authenticate_with_token, except: %i[index show]
+  before_action :authenticate_with_token
+
   before_action :set_hero, only: %i[show update destroy]
 
   # GET /heroes
+
   def index
-    @heroes = Hero.search(params[:term]).sorted_by_name
+    @heroes = Hero.by_token(@token).search(params[:term]).sorted_by_name
 
     render json: @heroes
   end
 
   # GET /heroes/1
+
   def show
     render json: @hero
   end
 
   # POST /heroes
+
   def create
-    @hero = Hero.new(hero_params)
+    @hero = Hero.new(hero_params.to_h.merge!({ token: @token }))
 
     if @hero.save
       render json: @hero, status: :created, location: api_hero_url(@hero)
@@ -28,6 +32,7 @@ class Api::HeroesController < ApplicationController
   end
 
   # PATCH/PUT /heroes/1
+
   def update
     if @hero.update(hero_params)
       render json: @hero
@@ -37,6 +42,7 @@ class Api::HeroesController < ApplicationController
   end
 
   # DELETE /heroes/1
+
   def destroy
     @hero.destroy
   end
@@ -44,11 +50,13 @@ class Api::HeroesController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
+
   def set_hero
-    @hero = Hero.find(params[:id])
+    @hero = Hero.by_token(@token).find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
+
   def hero_params
     params.require(:hero).permit(:name)
   end
